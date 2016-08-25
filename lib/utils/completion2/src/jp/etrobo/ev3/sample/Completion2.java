b244ea917735f741d5ac8d5f72779208946e1f92
kishila <s16g459@stu.kagawa-u.ac.jp>
2016-08-25 19:04:21 +0900

    スタート前の尻尾調節の機能追加

--- lib/utils/completion2/src/jp/etrobo/ev3/sample/Completion2.java
+++ lib/utils/completion2/src/jp/etrobo/ev3/sample/Completion2.java
@@ -10,6 +10,8 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
+import lejos.hardware.Button;
+import lejos.hardware.Sound;
 import lejos.hardware.lcd.LCD;
 import lejos.utility.Delay;
 
@@ -19,6 +21,7 @@ import lejos.utility.Delay;
 public class Completion2 {
     private EV3way         body;            // EV3 本体
     private boolean        touchPressed;    // タッチセンサーが押されたかの状態
+    private int standTailAngle;             // 倒立状態の尻尾の角度
 
     // スケジューラ
     private ScheduledExecutorService scheduler;
@@ -29,6 +32,9 @@ public class Completion2 {
     private EV3wayTask  driveTask;   // 走行制御
     private RemoteTask  remoteTask;  // リモート制御
 
+ // 非同期メソッド
+    private Thread call;
+
     /**
      * コンストラクタ。
      * スケジューラとタスクオブジェクトを作成。
@@ -38,11 +44,46 @@ public class Completion2 {
         body.idling();
         body.reset();
         touchPressed = false;
+        standTailAngle = EV3way.TAIL_ANGLE_STAND_UP;
 
         scheduler  = Executors.newScheduledThreadPool(2);
         driveTask  = new EV3wayTask(body);
         remoteTask = new RemoteTask();
         futureRemote = scheduler.scheduleAtFixedRate(remoteTask, 0, 100, TimeUnit.MILLISECONDS);
+
+        call = new Thread(() -> {
+    		Sound.playTone(400,300);
+    		Sound.playTone(450,300);
+    		Sound.playTone(800,300);
+    	});
+    }
+
+    /**
+     * スタート前の作業。
+     * 尻尾の角度を調整する
+     * @return true=wait / false=start
+     */
+    public boolean settingAngle(){
+        boolean res = true;
+        body.controlTail(standTailAngle);
+        if (body.touchSensorIsPressed()) {
+            touchPressed = true;          // タッチセンサーが押された
+        } else {
+            if (touchPressed) {
+                res = false;
+                touchPressed = false;     // タッチセンサーが押された後に放した
+            }
+        }
+        if (remoteTask.checkRemoteCommand(RemoteTask.REMOTE_COMMAND_START)) {  // PC で 'g' キーが押された
+            res = false;
+        }
+        if(Button.UP.isDown()){
+        	standTailAngle++;
+        } else if(Button.DOWN.isDown()) {
+        	standTailAngle--;
+        }
+        LCD.drawString(String.format("angle:%1$3d", standTailAngle), 0, 0);
+        return res;
     }
 
     /**
@@ -115,12 +156,26 @@ public class Completion2 {
     }
 
     /**
+     * 音で処理の完了を伝える
+     */
+    public void callSound() {
+    	call.start();
+    }
+
+
     /**
-     * メイン
+    /* メイン
      */
     public static void main(String[] args) {
     	LCD.drawString("Please Wait...  ", 0, 4);
 		Completion2 program = new Completion2();
+
+        // 尻尾の角度の調整
+        LCD.drawString("Setting Angle", 0, 4);
+        while (program.settingAngle()) {
+            Delay.msDelay(100);
+        }
+        program.callSound();
 
     	// スタート待ち
     	LCD.drawString("Touch to START", 0, 4);
